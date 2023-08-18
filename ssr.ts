aa984b6e31e463d3df11c561a0f8f4982d4465c4
KurtSt <89183354+KurtSt@users.noreply.github.com>
2023-08-18 12:28:45 +0200

    Für SSR zu ändernde Files
    Diese Files sind wichtig zum ändern von ssr https://inertiajs.com/server-side-rendering

---
+++ ssr.ts
@@ -0,0 +1,48 @@
+import { createSSRApp, h, DefineComponent } from 'vue';
+import { renderToString } from '@vue/server-renderer';
+import { createInertiaApp } from '@inertiajs/vue3';
+import createServer from '@inertiajs/vue3/server';
+import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
+import { ZiggyVue } from '../../vendor/tightenco/ziggy/dist/vue.m';
+
+// Vuetify and Vuetify Icons
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+import { aliases, fa } from 'vuetify/iconsets/fa'
+
+const vuetify = createVuetify({
+    icons: {
+        defaultSet: 'fa',
+        aliases,
+        sets: {
+            fa,
+        },
+    },
+    ssr: true,
+    components,
+    directives,
+})
+
+const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
+
+createServer((page) =>
+    createInertiaApp({
+        page,
+        render: renderToString,
+        title: (title) => `${title} - ${appName}`,
+        resolve: (name) => resolvePageComponent(`./Pages/${name}.vue`, import.meta.glob<DefineComponent>('./Pages/**/*.vue')),
+        setup({ App, props, plugin }) {
+            return createSSRApp({ render: () => h(App, props) })
+                .use(plugin)
+                .use(ZiggyVue, {
+                    // @ts-expect-error
+                    ...page.props.ziggy,
+                    // @ts-expect-error
+                    location: new URL(page.props.ziggy.location),
+                })
+                .use(vuetify);
+        },
+    })
+);
---
+++ vite.config.js
@@ -0,0 +1,26 @@
+import { defineConfig } from 'vite';
+import laravel from 'laravel-vite-plugin';
+import vue from '@vitejs/plugin-vue';
+import vuetify from "vite-plugin-vuetify";
+
+export default defineConfig({
+    plugins: [
+        laravel({
+            input: 'resources/js/app.ts',
+            ssr: 'resources/js/ssr.ts',
+            refresh: true,
+        }),
+        vue({
+            template: {
+                transformAssetUrls: {
+                    base: null,
+                    includeAbsolute: false,
+                },
+            },
+        }),
+        vuetify({ autoImport: true }),
+    ],
+     ssr: {
+  noExternal: [ /\.css$/, /^vuetify/ ],
+	},
+});
